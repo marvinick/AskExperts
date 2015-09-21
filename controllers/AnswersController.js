c4ad1af2bfba325c692317a060a540a7c6942573
marvinick <marviluw@gmail.com>
2015-09-20 21:28:36 -0700

    upvote

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,7 +1,11 @@
 askExperts.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionsFactory, UtilitiesFactory) {
     $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
     $scope.addAnswer = function() {
-      $scope.question.answers.push({ content: $scope.answerContent });
+      $scope.question.answers.push({ content: $scope.answerContent, upvotes: 0 });
       $scope.answerContent = null;
     }
+
+    $scope.incrementUpvotes = function(answer) {
+      $scope.answerContent.upvotes += 1;
+    };
 });
